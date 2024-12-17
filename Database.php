f2a2faa5f9d441712e182fa4bf35fe23a7b8afb2
MarijaGG <ipa23.m.gutane@vtdt.edu.lv>
2024-12-17 10:12:48 +0200

    Iesaaktais ar tikai children izvadu

---
+++ Database.php
@@ -0,0 +1,28 @@
+<?php
+
+class Database {
+
+    public $pdo;
+
+
+
+    public function __construct($config) {
+
+       // connect db with pdo
+        // dsn = data source name
+        $dsn = "mysql:" . http_build_query($config,"", ";");
+        // PHP data objekts
+        $this->pdo = new PDO($dsn);
+        $this->pdo->setAttribute(19, 2);
+
+    }
+
+    public function query($sql){
+
+        // sagatavot statement
+        $statement = $this->pdo->prepare($sql);
+        // do statement
+        $statement->execute(); 
+        return  $statement;
+
+    } }
---
+++ gifts.php
@@ -0,0 +1,28 @@
+<?php 
+
+
+require "Database.php";
+$config = require "config.php";
+
+
+$db = new Database($config["database"]);
+
+$children = $db->query("SELECT * FROM children")->fetchAll(PDO::FETCH_ASSOC);;
+// $comments = $db->query("SELECT * FROM comments")
+// $users = $db->query("SELECT * FROM users WHERE userid = $id")
+
+
+
+echo "<ol>";
+  foreach($children as $x) {
+    echo "<li>" . 
+        $x['firstname'] . " " . 
+        $x['middlename'] . " " . 
+        $x['surname'] . " " .
+        $x['age'] . 
+        "</li>"; }
+echo "</ol>";
+
+
+
+
