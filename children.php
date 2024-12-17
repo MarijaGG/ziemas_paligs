e9a62b1f888ad01154f4877ff6e4dab84fdba511
MarijaGG <ipa23.m.gutane@vtdt.edu.lv>
2024-12-17 11:25:37 +0200

    styles + stock list

--- children.php
+++ children.php
@@ -1,28 +1,48 @@
-<?php 
+<link rel="stylesheet" href="style.css">
 
+<?php 
 
 require "Database.php";
 $config = require "config.php";
 
-
 $db = new Database($config["database"]);
 
-$children = $db->query("SELECT * FROM children")->fetchAll(PDO::FETCH_ASSOC);;
-// $comments = $db->query("SELECT * FROM comments")
-// $users = $db->query("SELECT * FROM users WHERE userid = $id")
+// Iegūstam dāvanu nosaukumus no "gifts" tabulas
+$gifts = $db->query("SELECT name FROM gifts")->fetchAll(PDO::FETCH_ASSOC);
+$gift_names = array_column($gifts, 'name');
 
+// Iegūstam bērnu informāciju un vēstules tekstus
+$children = $db->query(
+    "SELECT children.firstname, children.middlename, children.surname, children.age, letters.letter_text 
+    FROM children
+    LEFT JOIN letters ON children.id = letters.sender_id"
+)->fetchAll(PDO::FETCH_ASSOC);
 
+echo "<div class='christmas-container'>";
+foreach ($children as $x) {
+    $letter_text = $x['letter_text'];
+    $wishes = [];
 
-echo "<ul>";
-  foreach($children as $x) {
-    echo "<li>" . 
-        $x['firstname'] . " " . 
-        $x['middlename'] . " " . 
-        $x['surname'] . " " .
-        $x['age'] . 
-        "</li>"; }
-echo "</ul>";
+    // Pārmeklējam vēstules tekstu un meklējam dāvanu nosaukumus
+    foreach ($gift_names as $gift) {
+        if (stripos($letter_text, $gift) !== false) {
+            // Ja dāvanu nosaukums ir atrasts, pievienojam to vēlmju sarakstam
+            $wishes[] = $gift;
+            // Highlighting the gift in the letter text
+            $letter_text = preg_replace("/\b" . preg_quote($gift, '/') . "\b/i", "<span class='highlight'>$0</span>", $letter_text);
+        }
+    }
 
+    // Izveidojam vēstules parādīšanu
+    echo "
+    <div class='christmas-box'>
+        <h3><span>Name:</span> " . htmlspecialchars($x['firstname']) . " " . htmlspecialchars($x['middlename']) . " " . htmlspecialchars($x['surname']) . "</h3>
+        <p><span>Age:</span> " . htmlspecialchars($x['age']) . "</p>
+        <p><span>Letter:</span> " . nl2br($letter_text) . "</p>"; // Do not apply htmlspecialchars here
 
+    // Remove the full wish list section
+    echo "</div>";
+}
+echo "</div>";
 
-
+?> 
--- gifts.php
+++ gifts.php
@@ -1,28 +1,60 @@
-<?php 
+<link rel="stylesheet" href="style.css">
 
+<?php
 
-require "Database.php";
-$config = require "config.php";
+require "Database.php"; // Include the Database class
+$config = require "config.php"; // Include your config file
 
-
+// Create a new Database object
 $db = new Database($config["database"]);
 
-$children = $db->query("SELECT * FROM children")->fetchAll(PDO::FETCH_ASSOC);;
-// $comments = $db->query("SELECT * FROM comments")
-// $users = $db->query("SELECT * FROM users WHERE userid = $id")
+// Fetch all gifts from the database
+$gifts = $db->query("SELECT * FROM gifts")->fetchAll(PDO::FETCH_ASSOC);
 
+// Create an array to hold the gift IDs and their request counts
+$gift_requests = [];
 
+// Fetch the number of children requesting each gift in one query
+$query = "
+    SELECT g.id AS gift_id, COUNT(l.id) AS request_count
+    FROM gifts g
+    LEFT JOIN letters l ON l.letter_text LIKE CONCAT('%', g.name, '%')
+    GROUP BY g.id
+";
 
+$results = $db->query($query)->fetchAll(PDO::FETCH_ASSOC);
+
+// Map the results to the $gift_requests array
+foreach ($results as $row) {
+    $gift_requests[$row['gift_id']] = $row['request_count'];
+}
+
+// Display the gift information in an ordered list
 echo "<ol>";
-  foreach($children as $x) {
+foreach ($gifts as $gift) {
+    // Get the number of children requesting this gift
+    $requests_count = $gift_requests[$gift['id']] ?? 0; // Default to 0 if not found
+
+    // Determine if there are enough gifts in stock
+    if ($gift['count_available'] < $requests_count) {
+        $availability = "Not enough stock!";
+        $status_class = "shortage";
+    } elseif ($gift['count_available'] > $requests_count) {
+        $availability = "Too many gifts!";
+        $status_class = "excess";
+    } else {
+        $availability = "Stock is sufficient!";
+        $status_class = "sufficient";
+    }
+
+    // Display the gift details, including stock and request info
     echo "<li>" . 
-        $x['firstname'] . " " . 
-        $x['middlename'] . " " . 
-        $x['surname'] . " " .
-        $x['age'] . 
-        "</li>"; }
+            htmlspecialchars($gift['name']) . " - " . 
+            htmlspecialchars($gift['count_available']) . " available | " .
+            $requests_count . " children want this gift. " .
+            "<span class='" . $status_class . "'>" . $availability . "</span>" .
+         "</li>";
+}
 echo "</ol>";
 
-
-
-
+?>
